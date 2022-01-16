916a7fa251201a7b81915b560cd89119e177c5ed
Michael Richter <70779298+RichterMJ@users.noreply.github.com>
2022-01-16 10:21:23 -0500

    Adding contact search api endpoint

---
+++ LAMPAPI/SearchContacts.php
@@ -0,0 +1,69 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "apiUser", "group9apiUser", "COP4331");
+
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("select * from Contacts where Name like ? and UserID=?");
+		$contactSearch = "%" . $inData["search"] . "%";
+		$stmt->bind_param("ss", $contactSearch, $inData["UserID"]);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '{"ID": "' . $row["ID"] . '","Name" : "'. $row["Name"] . '","PhoneNumber" : "'. $row["PhoneNumber"] .'","Email" : "'. $row["Email"] .'"}';
+		}
+
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>

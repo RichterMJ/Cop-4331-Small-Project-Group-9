54707dfc33af8481c5110172a7f3d9230fb37077
dangh2809 <84484451+dangh2809@users.noreply.github.com>
2022-01-19 17:00:27 -0500

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,53 +1,64 @@
+
 <?php
 
-$inData = getRequestInfo();
+    $inData = getRequestInfo();
+    
+    $id = 0;
+    $firstName = "";
+    $lastName = "";
 
-$id = 0;
-$firstName = "";
-$lastName = "";
+    $conn = new mysqli("localhost", "apiUser", "group9apiUser", "COP4331");
+    if( $conn->connect_error )
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM USERS WHERE Login=? AND Password=?");
+        $stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+      
+        if($row['COUNT(*)'] == 1)
+        {
+              $stmt = $conn->prepare("SELECT ID FROM USERS WHERE Login=? AND Password=?");
+            $stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
+            returnTrue($row['ID']);
+        }
+        else
+        {
+            returnFalse();
+        }
 
-$conn = new mysqli(getenv("DB_HOST"), getenv("API_USER"), getenv("API_PASS"), getenv("API_DB"));
-
-if ($conn->connect_error) {
-    returnWithError( $conn->connect_error );
-} else {
-    $stmt = $conn->prepare("SELECT ID ,FirstName, LastName FROM Users WHERE Login=? AND Password=?");
-    $stmt->bind_param("ss", $inData["login"], $inData["password"]);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-
-    if ($row = $result->fetch_assoc()) {
-        returnWithInfo($row['FirstName'], $row['LastName'], $row['ID']);
-    } else {
-        returnWithError("No Records Found");
+        $stmt->close();
+        $conn->close();
+    }
+    
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
     }
 
-    $stmt->close();
-    $conn->close();
-}
-
-function getRequestInfo()
-{
-    return json_decode(file_get_contents('php://input'), true);
-}
-
-function sendResultInfoAsJson($obj)
-{
-    header('Content-type: application/json');
-    echo $obj;
-}
-
-function returnWithError($err)
-{
-    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-    sendResultInfoAsJson($retValue);
-}
-
-function returnWithInfo($firstName, $lastName, $id)
-{
-    $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-    sendResultInfoAsJson($retValue);
-}
-
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    
+    function returnTrue($ID)
+    {
+        $retValue = '{"LoginSuccess":true, "ID": $ID}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
+    function returnFalse()
+    {
+        $retValue = '{"LoginSuccess":false}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
 ?>

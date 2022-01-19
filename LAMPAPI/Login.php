df9773c38ade22c81b99d848ed79cc9262cab235
Stephan Hartig <the.stephan.hartig@gmail.com>
2022-01-19 18:17:32 -0500

    merge

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <?php
 
@@ -54,69 +53,3 @@ function returnWithInfo($firstName, $lastName, $id)
 }
 
 ?>
-=======
-
-<?php
-
-	$inData = getRequestInfo();
-
-	$id = 0;
-	$firstName = "";
-	$lastName = "";
-
-	$conn = new mysqli("localhost", "apiUser", "group9apiUser", "COP4331");
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("select COUNT(*) from Users where Login=? AND Password=?");
-		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$row = $result->fetch_assoc();
-
-		if($row['COUNT(*)'] == 1)
-		{
-      $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=? AND Password=?");
-			$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
-			$stmt->execute();
-			$result = $stmt->get_result();
-			$row = $result->fetch_assoc();
-			returnTrue($row['ID']);
-		}
-		else
-		{
-			returnFalse();
-		}
-
-		$stmt->close();
-		$conn->close();
-	}
-
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-
-	function returnTrue($ID)
-	{
-		$retValue = '{"LoginSuccess": true, "ID": '.$ID.'}';
-		sendResultInfoAsJson( $retValue );
-	}
-
-	function returnFalse()
-	{
-		$retValue = '{"LoginSuccess":false}';
-		sendResultInfoAsJson( $retValue );
-	}
-
-?>
->>>>>>> 12a79caf6b1abc2113553de2b7e4634cd8b72ced

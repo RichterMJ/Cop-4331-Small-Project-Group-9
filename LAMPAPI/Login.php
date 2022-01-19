15af2bd48bae229349e54b10309d8576c0a31383
Michael Richter <70779298+RichterMJ@users.noreply.github.com>
2022-01-19 17:28:45 -0500

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,64 +1,64 @@
 
 <?php
 
-    $inData = getRequestInfo();
-    
-    $id = 0;
-    $firstName = "";
-    $lastName = "";
+	$inData = getRequestInfo();
 
-    $conn = new mysqli("localhost", "apiUser", "group9apiUser", "COP4331");
-    if( $conn->connect_error )
-    {
-        returnWithError( $conn->connect_error );
-    }
-    else
-    {
-        $stmt = $conn->prepare("SELECT COUNT(*) FROM USERS WHERE Login=? AND Password=?");
-        $stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-      
-        if($row['COUNT(*)'] == 1)
-        {
-              $stmt = $conn->prepare("SELECT ID FROM USERS WHERE Login=? AND Password=?");
-            $stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            $row = $result->fetch_assoc();
-            returnTrue($row['ID']);
-        }
-        else
-        {
-            returnFalse();
-        }
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
 
-        $stmt->close();
-        $conn->close();
-    }
-    
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+	$conn = new mysqli("localhost", "apiUser", "group9apiUser", "COP4331");
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("select COUNT(*) from Users where Login=? AND Password=?");
+		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$row = $result->fetch_assoc();
 
-    function sendResultInfoAsJson( $obj )
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
-    
-    function returnTrue($ID)
-    {
-        $retValue = '{"LoginSuccess":true, "ID": $ID}';
-        sendResultInfoAsJson( $retValue );
-    }
-    
-    function returnFalse()
-    {
-        $retValue = '{"LoginSuccess":false}';
-        sendResultInfoAsJson( $retValue );
-    }
-    
+		if($row['COUNT(*)'] == 1)
+		{
+      $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=? AND Password=?");
+			$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+			$stmt->execute();
+			$result = $stmt->get_result();
+			$row = $result->fetch_assoc();
+			returnTrue($row['ID']);
+		}
+		else
+		{
+			returnFalse();
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnTrue($ID)
+	{
+		$retValue = '{"LoginSuccess": true, "ID": '.$ID.'}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnFalse()
+	{
+		$retValue = '{"LoginSuccess":false}';
+		sendResultInfoAsJson( $retValue );
+	}
+
 ?>

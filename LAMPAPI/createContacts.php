1b69cb78718c8b032a2746ab8a0989664c61aa1c
Stephan Hartig <the.stephan.hartig@gmail.com>
2022-01-19 13:43:07 -0500

    Adding what we have to the github

---
+++ LAMPAPI/createContacts.php
@@ -0,0 +1,44 @@
+<?php
+   $inData = getRequestInfo();
+   
+   
+    $name = $inData[""];
+    $phoneNumber = $inData["PhoneNumber"];
+    $email = $inData["Email"];
+    $userId = $inData["UserID"];
+
+
+   $conn = new mysqli("localhost", "apiUser", "group9apiUser", "COP4331");
+   if ($conn->connect_error)
+   {
+       returnWithError( $conn->connect_error );
+   }
+   else
+   {
+       $stmt = $conn->prepare("INSERT into Contacts(Name,PhoneNumber,Email,UserID) VALUES(?,?,?,?)");
+       $stmt->bind_param("ssss", $name, $phoneNumber, $email, $userId);
+       $stmt->execute();
+       $stmt->close();
+       $conn->close();
+       returnWithError("");
+   }
+
+   function getRequestInfo()
+   {
+       return json_decode(file_get_contents('php://input'), true);
+   }
+
+   function sendResultInfoAsJson( $obj )
+   {
+       header('Content-type: application/json');
+       echo $obj;
+   }
+   
+   function returnWithError( $err )
+   {
+       $retValue = '{"error":"' . $err . '"}';
+       sendResultInfoAsJson( $retValue );
+   }
+   
+?>
+

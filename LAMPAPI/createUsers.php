de1cfac8a01231a218e9db076b092b05f06a145f
dangh2809 <84484451+dangh2809@users.noreply.github.com>
2022-01-20 16:02:43 -0500

    Update createUsers.php

--- LAMPAPI/createUsers.php
+++ LAMPAPI/createUsers.php
@@ -2,12 +2,12 @@
 
 $inData = getRequestInfo();
 
-$isUsernameTaken = 0;
+$isUsernameTaken =0;
 $firstName = $inData["FirstName"];
 $lastName = $inData["LastName"];
 $login = $inData["Login"];
 $pass = $inData["Password"];
-
+$err = "";
 
 # Doesn't seem to want to work =(
 #$conn = new mysqli(getenv("API_HOST"), getenv("API_USER"), getenv("API_PASS"), getenv("API_DB"));
@@ -23,13 +23,13 @@ if ($conn->connect_error) {
     $result = $stmt->get_result();
     if ($result->num_rows > 0) {
       $isUsernameTaken = 1;
-      returnWithResult("Username has already taken");
+      returnWithResult($isUsernameTaken, $firstName, $lastName, $login, $pass, $error = "Username is already taken");
     }
     else {
       $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
       $stmt->bind_param("ssss",$firstName, $lastName, $login, $pass);
       $stmt->execute();
-      returnWithResult($isUsernameTaken, $firstName, $lastName, $login, $pass);
+      returnWithResult($isUsernameTaken, $firstName, $lastName, $login, $pass, $err);
     }
     $stmt->close();
     $conn->close();
@@ -40,9 +40,9 @@ function getRequestInfo()
 {
     return json_decode(file_get_contents('php://input'), true);
 }
-function returnWithResult($isUsernameTaken, $firstName, $lastName, $login, $password)
+function returnWithResult($isUsernameTaken, $firstName, $lastName, $login, $password, $err)
 {
-  $retVal = '{"isUsernameTaken" : ' . $isUsernameTaken . ',"firstName" : "' . $firstName . '", "lastName" : "' . $lastName . '", "login" : "' . $login . '","password" : "' . $password . '"}';
+  $retVal = '{"isUsernameTaken" :' . $isUsernameTaken . ',"firstName" : "' . $firstName . '", "lastName" : "' . $lastName . '", "login" : "' . $login . '","password" : "' . $password . '","error" : "' . $err . '"}';
   sendResultInfoAsJson($retVal);
 }
 function sendResultInfoAsJson($obj)

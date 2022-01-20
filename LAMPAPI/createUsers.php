2d4ed83ef403e646b21fddfb717f79e6ac01ad4f
dangh2809 <84484451+dangh2809@users.noreply.github.com>
2022-01-20 02:53:22 -0500

    Update createUsers.php

--- LAMPAPI/createUsers.php
+++ LAMPAPI/createUsers.php
@@ -12,19 +12,34 @@ $conn = new mysqli(getenv("DB_HOST"), getenv("API_USER"), getenv("API_PASS"), ge
 if ($conn->connect_error) {
     returnWithError( $conn->connect_error );
 } else {
-    $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss",$firstName, $lastName, $login, $pass);
+    $stmt = $conn->prepare("SELECT Login FROM Users WHERE Login=?");
+    $stmt->bind_param("s", $login);
     $stmt->execute();
+    $result = stmt->get_result();
+    if ($result->num_rows > 0) {
+      returnWithError("Username has already taken");
+    }
+    else {
+      $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+      $stmt->bind_param("ssss",$firstName, $lastName, $login, $pass);
+      $stmt->execute();
+      sendResultInfoAsJson($firstName, $lastName, $login, $pass);
+    }
     $stmt->close();
     $conn->close();
     returnWithError("");
+
 }
 
 function getRequestInfo()
 {
     return json_decode(file_get_contents('php://input'), true);
 }
-
+function returnWithResult($firstname, $lastName, $login, $password)
+{
+  $retVal = '{"firstName" : "' . $firstname . '", "lastName" : "' . $lastName . '", "login" : "' . $login . '","password" : "' . $password . '"}';
+  sendResultInfoAsJson($retVal);
+}
 function sendResultInfoAsJson($obj)
 {
     header('Content-type: application/json');
@@ -38,4 +53,3 @@ function returnWithError($err)
 }
 
 ?>
-

99d1dbfe6d27875a240ed3d8d6e4279a57ff050b
Stephan Hartig <the.stephan.hartig@gmail.com>
2022-01-21 10:37:08 -0500

    Added search contacts functionality

--- scripts/contacts-script.js
+++ scripts/contacts-script.js
@@ -12,6 +12,57 @@ window.onload = function() {
     document.getElementById('welcome').innerHTML = `Welcome, ${FirstName} ${LastName}`;
 }
 
+/* Who needs to escape data anyways? */
+function convertContactsToTable(contacts) {
+    return `
+        <table class="table thead-dark table-striped">
+            <thead>
+                <tr>
+                    <th style="width: 33.33%">Name</td>
+                    <th style="width: 33.33%">Phone number</td>
+                    <th style="width: 33.33%">Email</td>
+                </tr>
+            </thead>
+            <tbody>
+                ${contacts.map(convertContactToTableRow).join('')}
+            </tbody>
+        </table>
+    `;
+}
+
+function convertContactToTableRow(contact) {
+    return `
+        <tr>
+            <td style="width: 33.33%">${contact.Name}</td>
+            <td style="width: 33.33%">${contact.PhoneNumber}</td>
+            <td style="width: 33.33%">${contact.Email}</td>
+        </tr>
+    `;
+}
+
+async function searchContact() {
+
+    const search = document.getElementById('searchQuery').value;
+
+    const res = await fetch('/LAMPAPI/SearchContacts.php', {
+        method: 'POST',
+        body: JSON.stringify({ search, UserID }),
+    });
+
+    if (!res.ok) {
+        document.getElementById('searchResults').innerHTML = 'There was an error connecting to the server, try again later.';
+    }
+
+    const resJson = await res.json();
+
+    if (resJson.error !== '') {
+        document.getElementById('searchResults').innerHTML = resJson.error;
+    } else {
+        document.getElementById('searchResults').innerHTML = convertContactsToTable(resJson.results);
+    }
+
+}
+
 async function addContact() {
 
     const Name = document.getElementById('name').value;

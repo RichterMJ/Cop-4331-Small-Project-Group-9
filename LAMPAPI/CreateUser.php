3bd683dc3787d1b93919367e77543b35b8a5eb18
Michael Richter <70779298+RichterMJ@users.noreply.github.com>
2022-01-21 14:28:53 -0500

    Update CreateUser.php

--- LAMPAPI/CreateUser.php
+++ LAMPAPI/CreateUser.php
@@ -2,12 +2,10 @@
 
 $inData = getRequestInfo();
 
-$isUsernameTaken =0;
 $firstName = $inData["FirstName"];
 $lastName = $inData["LastName"];
 $login = $inData["Login"];
 $pass = $inData["Password"];
-$err = "";
 
 # Doesn't seem to want to work =(
 #$conn = new mysqli(getenv("API_HOST"), getenv("API_USER"), getenv("API_PASS"), getenv("API_DB"));
@@ -16,20 +14,14 @@ $conn = new mysqli('localhost', 'apiUser', 'group9apiUser', 'COP4331');
 if ($conn->connect_error) {
     returnWithError( $conn->connect_error );
 } else {
-    $stmt = $conn->prepare("SELECT Login FROM Users WHERE Login=?");
-    $stmt->bind_param("s", $login);
-
+    $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss",$firstName, $lastName, $login, $pass);
     $stmt->execute();
-    $result = $stmt->get_result();
-    if ($result->num_rows > 0) {
-      $isUsernameTaken = 1;
-      returnWithResult($isUsernameTaken, $firstName, $lastName, $login, $pass, $error = "Username is already taken");
-    }
-    else {
-      $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
-      $stmt->bind_param("ssss",$firstName, $lastName, $login, $pass);
-      $stmt->execute();
-      returnWithResult($isUsernameTaken, $firstName, $lastName, $login, $pass, $err);
+
+    if ($stmt->affected_rows == 1) {
+      returnWithError("");
+    } else {
+      returnWithError('Username ' . $login . ' is already taken.');
     }
     $stmt->close();
     $conn->close();
@@ -40,9 +32,11 @@ function getRequestInfo()
 {
     return json_decode(file_get_contents('php://input'), true);
 }
-function returnWithResult($isUsernameTaken, $firstName, $lastName, $login, $password, $err)
+
+#Just return error
+function returnNoError()
 {
-  $retVal = '{"isUsernameTaken" :' . $isUsernameTaken . ',"firstName" : "' . $firstName . '", "lastName" : "' . $lastName . '", "login" : "' . $login . '","password" : "' . $password . '","error" : "' . $err . '"}';
+  $retVal = '{"error":""}';
   sendResultInfoAsJson($retVal);
 }
 function sendResultInfoAsJson($obj)

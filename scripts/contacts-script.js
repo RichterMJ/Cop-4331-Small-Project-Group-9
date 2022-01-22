cfafbca6c803df2634933bb09afd16d2e2e5fa07
kylemckibben <kyleamckibben@gmail.com>
2022-01-22 17:14:19 -0500

    Frontend update (fixed responsiveness issues contacts page), added logout button

--- scripts/contacts-script.js
+++ scripts/contacts-script.js
@@ -3,85 +3,82 @@ let lastestSearch = '';
 
 const DEBUG = false;
 
-window.onload = function() {
-    if (DEBUG) {
-        UserID    = 14;
-        FirstName = 'Bill';
-        LastName  = 'Marrow';
+window.onload = function () {
+	if (DEBUG) {
+		UserID = 14;
+		FirstName = 'Bill';
+		LastName = 'Marrow';
 
-        document.getElementById('searchResults').innerHTML = convertContactsToTable(getMockContacts());
-    } else {
-        // So secure <3
-        UserID = parseInt(getCookieVal('UserID'));
-        FirstName = getCookieVal('FirstName');
-        LastName = getCookieVal('LastName');
-    }
+		document.getElementById('search-results').innerHTML = convertContactsToTable(getMockContacts());
+	} else {
+		// So secure <3
+		UserID = parseInt(getCookieVal('UserID'));
+		FirstName = getCookieVal('FirstName');
+		LastName = getCookieVal('LastName');
+	}
 
-    if (UserID == null) {
-        window.location.href = '/';
-    }
+	if (UserID == null) {
+		window.location.href = '/';
+	}
 
-    document.getElementById('welcome').innerHTML = `Welcome, ${FirstName} ${LastName}`;
+	document.getElementById('welcome').innerHTML = `Welcome, ${FirstName} ${LastName}`;
 
 }
 
 /* Who needs to escape data anyways? */
 function convertContactsToTable(contacts) {
-    return `
-        <div class="forms text-center p-5">
-			<table class="table thead-dark table-striped">
-				<thead>
-					<tr>
-						<th style="width: 25%">Name</td>
-						<th style="width: 25%">Phone number</td>
-						<th style="width: 25%">Email</td>
-						<th style="width: 10%"></td>
-						<th style="width: 15%"></td>
-					</tr>
-				</thead>
-				<tbody>
-					${contacts.map(convertContactToTableRow).join('')}
-				</tbody>
-			</table>
+	return `
+		<div class="container text-center p-5" style="border: 1px solid black;">
+			<div class="row p-1">
+				<div class="col-xl-2">Name</div>
+				<div class="col-xl-2">Phone #</div>
+				<div class="col-xl-4">Email</div>
+				<div class="col-xl-2 pt-2"></div>
+				<div class="col-xl-2 pt-2"></div>
+			</div>
+			<hr>
+			<div>
+				${contacts.map(convertContactToTableRow).join('')}
+			</div>
 		</div>
     `;
 }
 
 function convertContactToTableRow(contact) {
-    return `
-        <tr>
-            <td style="width: 25%">${contact.Name}</td>
-            <td style="width: 25%">${contact.PhoneNumber}</td>
-            <td style="width: 25%">${contact.Email}</td>
-            <td style="width: 10%">${generateUpdateButton(contact)}</td>
-            <td style="width: 15%">${generateDeleteButton(contact)}</td>
-        </tr>
+	return `
+		<div class="contacts row p-1">
+			<div class="col-xl-2">${contact.Name}</div>
+			<div class="col-xl-2">${contact.PhoneNumber}</div>
+			<div class="col-xl-4">${contact.Email}</div>
+			<div class="col-xl-2 py-2">${generateUpdateButton(contact)}</div>
+			<div class="col-xl-2 py-2">${generateDeleteButton(contact)}</div>
+		</div>
     `;
 }
 
 function generateUpdateButton(contact) {
-    return `
-        <button type="button" class="btn btn-secondary btn-sm btn-block" onclick='editContact(${JSON.stringify(contact)})'>Edit</button>
+	return `
+		<button class="btn btn-sm btn-block btn-secondary" onclick='editContact(${JSON.stringify(contact)})'>Edit</button>
     `;
 }
 
 function generateDeleteButton(contact) {
-    return `
-        <button type="button" class="btn btn-danger btn-sm btn-block" onclick="deleteContact(${contact.ID})">Delete</button>
+	return `
+		<button class="btn btn-sm btn-block btn-danger" onclick="deleteContact(${contact.ID})">Delete</button>
     `;
 }
 
 function getMockContacts() {
-    return [
-        { ID: 1, Name: 'one', PhoneNumber: '111111', Email: 'one@one' },
-        { ID: 2, Name: 'two', PhoneNumber: '222222', Email: 'two@two' },
-        { ID: 3, Name: 'three', PhoneNumber: '333333', Email: 'three@three' },
-        { ID: 4, Name: 'four', PhoneNumber: '444444', Email: 'four@four' },
-    ];
+	return [
+		{ ID: 1, Name: 'one', PhoneNumber: '111111', Email: 'one@one' },
+		{ ID: 2, Name: 'two', PhoneNumber: '222222', Email: 'two@two' },
+		{ ID: 3, Name: 'three', PhoneNumber: '333333', Email: 'three@three' },
+		{ ID: 4, Name: 'four', PhoneNumber: '444444', Email: 'four@four' },
+	];
 }
 
 function editContact(contact) {
-    document.getElementById('editContactFormGoesHere').innerHTML = `
+	document.getElementById('editContactFormGoesHere').innerHTML = `
             <!-- Edit contact form. -->
             <form id="editContactForm" class="forms container text-center py-5">
                 <h2 class="m-3">Edit Contact Form</h2>
@@ -133,118 +130,118 @@ function editContact(contact) {
             </form>
     `;
 
-    document.getElementById('editFromName').value = contact.Name;
-    document.getElementById('editFromPhonenumber').value = contact.PhoneNumber;
-    document.getElementById('editFromEmail').value = contact.Email;
+	document.getElementById('editFromName').value = contact.Name;
+	document.getElementById('editFromPhonenumber').value = contact.PhoneNumber;
+	document.getElementById('editFromEmail').value = contact.Email;
 
-    document.getElementById('editToName').value = contact.Name;
-    document.getElementById('editToPhonenumber').value = contact.PhoneNumber;
-    document.getElementById('editToEmail').value = contact.Email;
+	document.getElementById('editToName').value = contact.Name;
+	document.getElementById('editToPhonenumber').value = contact.PhoneNumber;
+	document.getElementById('editToEmail').value = contact.Email;
 
-    document.getElementById('editContactForm').scrollIntoView({ block: 'center',  behavior: 'smooth' });
+	document.getElementById('editContactForm').scrollIntoView({ block: 'center', behavior: 'smooth' });
 }
 
 /* Call the API to update a contact. Must supply `ContactID` as an argument. All other data is retrieved from the DOM */
 async function updateContact(ContactID) {
 
-    const Name = document.getElementById('editToName').value;
-    const PhoneNumber = document.getElementById('editToPhonenumber').value;
-    const Email = document.getElementById('editToEmail').value;
+	const Name = document.getElementById('editToName').value;
+	const PhoneNumber = document.getElementById('editToPhonenumber').value;
+	const Email = document.getElementById('editToEmail').value;
 
-    const res = await fetch('LAMPAPI/UpdateContact.php', {
-        method: 'POST',
-        body: JSON.stringify({ ContactID, Name, PhoneNumber, Email }),
-    });
+	const res = await fetch('LAMPAPI/UpdateContact.php', {
+		method: 'POST',
+		body: JSON.stringify({ ContactID, Name, PhoneNumber, Email }),
+	});
 
-    if (!res.ok) {
-        document.getElementById('editResults').innerHTML = 'There was an error connecting to the server, try again later. No contacts deleted.';
-    }
+	if (!res.ok) {
+		document.getElementById('editResults').innerHTML = 'There was an error connecting to the server, try again later. No contacts deleted.';
+	}
 
-    const resJson = await res.json();
+	const resJson = await res.json();
 
-    if (resJson.error !== '') {
-        document.getElementById('editResults').innerHTML = resJson.error;
-    } else {
-        searchContact(lastestSearch);
-        document.getElementById('editContactFormGoesHere').innerHTML = '';
-        document.getElementById('searchResults').scrollIntoView({ block: 'center',  behavior: 'smooth' });
-    }
+	if (resJson.error !== '') {
+		document.getElementById('editResults').innerHTML = resJson.error;
+	} else {
+		searchContact(lastestSearch);
+		document.getElementById('editContactFormGoesHere').innerHTML = '';
+		document.getElementById('searchResults').scrollIntoView({ block: 'center', behavior: 'smooth' });
+	}
 }
 
 /* Call the API to delete a contact. Must supply `ContactID` as an argument. */
 async function deleteContact(ContactID) {
-    
-    const res = await fetch('LAMPAPI/DeleteContact.php', {
-        method: 'POST',
-        body: JSON.stringify({ ContactID }),
-    });
 
-    if (!res.ok) {
-        document.getElementById('searchResults').innerHTML = 'There was an error connecting to the server, try again later. No contacts deleted.';
-    }
+	const res = await fetch('LAMPAPI/DeleteContact.php', {
+		method: 'POST',
+		body: JSON.stringify({ ContactID }),
+	});
 
-    const resJson = await res.json();
+	if (!res.ok) {
+		document.getElementById('searchResults').innerHTML = 'There was an error connecting to the server, try again later. No contacts deleted.';
+	}
 
-    if (resJson.error !== '') {
-        document.getElementById('searchResults').innerHTML = resJson.error;
-    } else {
-        searchContact(lastestSearch);
-    }
+	const resJson = await res.json();
+
+	if (resJson.error !== '') {
+		document.getElementById('searchResults').innerHTML = resJson.error;
+	} else {
+		searchContact(lastestSearch);
+	}
 }
 
 /* Call the API to search for contacts. Can either supply the search term or will automatically get from the DOM. */
 async function searchContact(search) {
 
-    if (typeof search === 'undefined')
-        search = document.getElementById('searchQuery').value;
+	if (typeof search === 'undefined')
+		search = document.getElementById('searchQuery').value;
 
-    lastestSearch = search;
+	lastestSearch = search;
 
-    const res = await fetch('/LAMPAPI/SearchContacts.php', {
-        method: 'POST',
-        body: JSON.stringify({ search, UserID }),
-    });
+	const res = await fetch('/LAMPAPI/SearchContacts.php', {
+		method: 'POST',
+		body: JSON.stringify({ search, UserID }),
+	});
 
-    if (!res.ok) {
-        document.getElementById('searchResults').innerHTML = 'There was an error connecting to the server, try again later.';
-    }
+	if (!res.ok) {
+		document.getElementById('searchResults').innerHTML = 'There was an error connecting to the server, try again later.';
+	}
 
-    const resJson = await res.json();
+	const resJson = await res.json();
 
-    if (resJson.error !== '') {
-        document.getElementById('searchResults').innerHTML = resJson.error;
-    } else {
-        document.getElementById('searchResults').innerHTML = convertContactsToTable(resJson.results);
-    }
+	if (resJson.error !== '') {
+		document.getElementById('searchResults').innerHTML = resJson.error;
+	} else {
+		document.getElementById('searchResults').innerHTML = convertContactsToTable(resJson.results);
+	}
 
 }
 
 /* Call the API to add a contact. Will automatically get data from the DOM. */
 async function addContact() {
 
-    const Name = document.getElementById('name').value;
-    const PhoneNumber = document.getElementById('phoneNumber').value;
-    const Email = document.getElementById('email').value;
+	const Name = document.getElementById('name').value;
+	const PhoneNumber = document.getElementById('phoneNumber').value;
+	const Email = document.getElementById('email').value;
 
-    const res = await fetch('/LAMPAPI/CreateContact.php', {
-        method: 'POST',
-        body: JSON.stringify({ Name, PhoneNumber, Email, UserID }),
-    });
+	const res = await fetch('/LAMPAPI/CreateContact.php', {
+		method: 'POST',
+		body: JSON.stringify({ Name, PhoneNumber, Email, UserID }),
+	});
 
-    if (!res.ok) {
-        document.getElementById('addContactResult').innerHTML = 'There was an error connecting to the server, try again later.';
-    }
+	if (!res.ok) {
+		document.getElementById('addContactResult').innerHTML = 'There was an error connecting to the server, try again later.';
+	}
 
-    const resJson = await res.json();
+	const resJson = await res.json();
 
-    if (resJson.error !== '') {
-        document.getElementById('addContactResult').innerHTML = resJson.error;
-    } else {
-        document.getElementById('addContactResult').innerHTML = 'Sign up successful!';
-    }
+	if (resJson.error !== '') {
+		document.getElementById('addContactResult').innerHTML = resJson.error;
+	} else {
+		document.getElementById('addContactResult').innerHTML = 'Sign up successful!';
+	}
 
-    document.getElementById('name').value = '';
-    document.getElementById('phoneNumber').value = '';
-    document.getElementById('email').value = '';
+	document.getElementById('name').value = '';
+	document.getElementById('phoneNumber').value = '';
+	document.getElementById('email').value = '';
 }
 
--- scripts/login-script.js
+++ scripts/login-script.js
@@ -2,69 +2,69 @@ const urlBase = 'http://cops43319.xyz';
 
 async function signIn() {
 
-    const login = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+	const login = document.getElementById('username').value;
+	const password = document.getElementById('password').value;
 
-    document.getElementById('loginResult').innerHTML = '';
-
-
-    const res = await fetch('/LAMPAPI/LoginUser.php', {
-        method: 'POST',
-        body: JSON.stringify({ Login: login, Password: md5(password) }),
-    });
-
-    console.log('hashed pass=' + md5(password));
-
-    if (!res.ok) {
-        document.getElementById('loginResult').innerHTML = 'There was an error connecting to the server, try again later.';
-    }
-
-    const resJson = await res.json();
-
-    if (resJson.error !== '') {
-        document.getElementById('loginResult').innerHTML = 'User/Password combination incorrect';
-        console.log('An error was encountered while trying to login.');
-        console.log(resJson.error);
-        return;
-    } else {
-        document.getElementById('loginResult').innerHTML = 'Sign in successful!';
-
-        saveCookie({ UserID: resJson.id, FirstName: resJson.firstName, LastName: resJson.lastName });
-        window.location.replace('/contacts.html');
-    }
+	document.getElementById('loginResult').innerHTML = '';
 
 
-//    const jsonPayload = JSON.stringify({
-//        Login: login,
-//        Password: md5(password),
-//    });
-//
-//    const loginAPI = urlBase + '/LAMPAPI/Login.php'
-//
-//    const xhr = new XMLHttpRequest();
-//    xhr.open('POST', loginAPI, true);
-//    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-//
-//    try {
-//        xhr.onreadystatechange = () => {
-//            if (xhr.readyState == 4 && xhr.status == 200) {
-//                const res = JSON.parse(xhr.responseText);
-//
-//                if (res.error !== '') {
-//                    document.getElementById('loginResult').innerHTML = 'User/Password combination incorrect';
-//                    console.log('An error was encountered while trying to login.');
-//                    return;
-//                }
-//
-//                saveCookie({ UserID: res.id, FirstName: res.firstName, LastName: res.lastName });
-//                window.location.href = urlBase + '/contacts.html';
-//            }
-//        };
-//
-//        xhr.send(jsonPayload);
-//    } catch (err) {
-//        document.getElementById('loginResult').innerHTML = err.message;
-//        console.log('An error was CAUGHT while attempting login.');
-//    }
+	const res = await fetch('/LAMPAPI/LoginUser.php', {
+		method: 'POST',
+		body: JSON.stringify({ Login: login, Password: md5(password) }),
+	});
+
+	console.log('hashed pass=' + md5(password));
+
+	if (!res.ok) {
+		document.getElementById('loginResult').innerHTML = 'There was an error connecting to the server, try again later.';
+	}
+
+	const resJson = await res.json();
+
+	if (resJson.error !== '') {
+		document.getElementById('loginResult').innerHTML = 'User/Password combination incorrect';
+		console.log('An error was encountered while trying to login.');
+		console.log(resJson.error);
+		return;
+	} else {
+		document.getElementById('loginResult').innerHTML = 'Sign in successful!';
+
+		saveCookie({ UserID: resJson.id, FirstName: resJson.firstName, LastName: resJson.lastName });
+		window.location.replace('/contacts.html');
+	}
+
+
+	//    const jsonPayload = JSON.stringify({
+	//        Login: login,
+	//        Password: md5(password),
+	//    });
+	//
+	//    const loginAPI = urlBase + '/LAMPAPI/Login.php'
+	//
+	//    const xhr = new XMLHttpRequest();
+	//    xhr.open('POST', loginAPI, true);
+	//    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+	//
+	//    try {
+	//        xhr.onreadystatechange = () => {
+	//            if (xhr.readyState == 4 && xhr.status == 200) {
+	//                const res = JSON.parse(xhr.responseText);
+	//
+	//                if (res.error !== '') {
+	//                    document.getElementById('loginResult').innerHTML = 'User/Password combination incorrect';
+	//                    console.log('An error was encountered while trying to login.');
+	//                    return;
+	//                }
+	//
+	//                saveCookie({ UserID: res.id, FirstName: res.firstName, LastName: res.lastName });
+	//                window.location.href = urlBase + '/contacts.html';
+	//            }
+	//        };
+	//
+	//        xhr.send(jsonPayload);
+	//    } catch (err) {
+	//        document.getElementById('loginResult').innerHTML = err.message;
+	//        console.log('An error was CAUGHT while attempting login.');
+	//    }
 }
 

afa006312077b8a8a583dbce7ca4c2cc08e3bdb3
Stephan Hartig <the.stephan.hartig@gmail.com>
2022-01-27 14:55:32 -0500

    Updated index.html (login page) and signup.html to the new style

--- scripts/signup-script.js
+++ scripts/signup-script.js
@@ -1,22 +1,44 @@
+
+function complainAboutMismatchedPasswords() {
+	document.getElementById('password-repeat').classList.add('is-invalid');
+}
+
+function uncomplainAboutMismatchedPasswords() {
+	document.getElementById('password-repeat').classList.remove('is-invalid');
+}
+
+function handlePasswordRepeatInput() {
+    const password = document.getElementById('password').value;
+    const passwordRepeat = document.getElementById('password-repeat').value;
+
+	if (password !== passwordRepeat) {
+		complainAboutMismatchedPasswords();
+	} else {
+		uncomplainAboutMismatchedPasswords();
+	}
+}
+
 async function signUp() {
 
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const passwordRepeat = document.getElementById('password-repeat').value;
 
-    const signupAPI = '/LAMPAPI/CreateUser.php';
+	if (password !== passwordRepeat) {
+		complainAboutMismatchedPasswords();
+		return;
+	}
 
-    const jsonPayload = JSON.stringify({
-        FirstName: firstName,
-        LastName: lastName,
-        Login: username,
-        Password: md5(password),
-    });
-
-    const res = await fetch(signupAPI, {
+    const res = await fetch('/LAMPAPI/CreateUser.php', {
         method: 'POST',
-        body: jsonPayload,
+		body: {
+			FirstName: firstName,
+			LastName: lastName,
+			Login: username,
+			Password: md5(password),
+		}
     });
 
     if (!res.ok) {

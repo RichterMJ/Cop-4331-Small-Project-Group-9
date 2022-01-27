c2c3b97a84b319f1e7314f34a010d565094a3603
Stephan Hartig <the.stephan.hartig@gmail.com>
2022-01-27 15:25:28 -0500

    Fixed bug on login page and signup page

--- scripts/signup-script.js
+++ scripts/signup-script.js
@@ -1,9 +1,30 @@
+function complainAboutBlank(htmlId) {
+	document.getElementById('signupResult').innerHTML = 'One or more fields are blank.';
+	document.getElementById(htmlId).classList.add('is-invalid');
+}
+
+function uncomplainAboutBlank(htmlId) {
+	document.getElementById('signupResult').innerHTML = '';
+	document.getElementById(htmlId).classList.remove('is-invalid');
+}
+
+function handleBlankCheck(htmlId) {
+    const content = document.getElementById(htmlId).value;
+
+	if (content === '') {
+		complainAboutBlank(htmlId);
+	} else {
+		uncomplainAboutBlank(htmlId);
+	}
+}
 
 function complainAboutMismatchedPasswords() {
+		document.getElementById('signupResult').innerHTML = 'Passwords do not match.';
 	document.getElementById('password-repeat').classList.add('is-invalid');
 }
 
 function uncomplainAboutMismatchedPasswords() {
+	document.getElementById('signupResult').innerHTML = '';
 	document.getElementById('password-repeat').classList.remove('is-invalid');
 }
 
@@ -26,9 +47,20 @@ async function signUp() {
     const password = document.getElementById('password').value;
     const passwordRepeat = document.getElementById('password-repeat').value;
 
+	let anyBlank = false;
+	for (const htmlId of ['firstName', 'lastName', 'username', 'password']) {
+		handleBlankCheck(htmlId);
+		if (document.getElementById(htmlId).value === '') {
+			anyBlank = true;
+		}
+	}
+	if (anyBlank) {
+		document.getElementById('signupResult').innerHTML = 'One or more fields are blank.';
+		return;
+	}
+
 	if (password !== passwordRepeat) {
 		complainAboutMismatchedPasswords();
-		document.getElementById('signupResult').innerHTML = 'Passwords do not match!';
 		return;
 	}
 

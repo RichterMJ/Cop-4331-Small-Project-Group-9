b3ab5a6f84d1c194dd49b0dab61e42d357b98627
Stephan Hartig <the.stephan.hartig@gmail.com>
2022-02-03 18:48:29 -0500

    Added photo

---
+++ docs/example-tables/Example.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO COP4331.Users
+	(FirstName, LastName, Login, Password)
+VALUES
+	("Andrew", "Smith", "andy@example.com", "d8578edf8458ce06fbc5bb76a58c5ca4"), -- Password=md5("qwerty")
+	("Robert", "Calle", "robb@example.com", "d8578edf8458ce06fbc5bb76a58c5ca4"), -- Password=md5("qwerty")
+	("Joseph", "Mears", "joey@example.com", "d8578edf8458ce06fbc5bb76a58c5ca4"), -- Password=md5("qwerty")
+	("", "", "", "d8578edf8458ce06fbc5bb76a58c5ca4"), -- Password=md5("qwerty")
+	("", "", "", "d8578edf8458ce06fbc5bb76a58c5ca4"), -- Password=md5("qwerty")
+;
+
